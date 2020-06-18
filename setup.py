b14266e7b5bcbc5bc8fc7c80468c04b37019d75e
Andrew Smith <andrewgsmith@gmail.com>
2020-06-17 21:57:36 -0400

    add python module to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name="mkchain",
     version="0.1",
+    py_modules=['mkchain'],
     install_requires=["pyyaml", "kubernetes"],
     entry_points={"console_scripts": ["mkchain=mkchain:main"]},
 )
