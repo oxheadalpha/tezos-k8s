dec7345a96e42a781e27230c3cb36e10f6d7845d
Aryeh Harris <harryttd@gmail.com>
2020-10-27 16:18:18 -0400

    WIP

--- rpc-auth/client/index.sh
+++ rpc-auth/client/index.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# set -e
-set -eo pipefail
-
 while test $# -gt 0; do
   case "$1" in
   -h | --help)
@@ -49,52 +46,60 @@ while test $# -gt 0; do
   esac
 done
 
-
-if [ -z "$CLUSTER_ADDRESS" ]
-then
+if [ -z "$CLUSTER_ADDRESS" ]; then
   echo "--cluster-address flag is required"
   exit 1
-elif [ -z "$CHAIN_ID" ]
-then
+elif [ -z "$CHAIN_ID" ]; then
   echo "--chain-id flag is required"
   exit 1
-elif [ -z "$TZ_ALIAS" ]
-then
+elif [ -z "$TZ_ALIAS" ]; then
   echo "--tz-alias flag is required"
   exit 1
 fi
 
-if ! tezos-client show address $TZ_ALIAS >/dev/null 2>&1
-then
- echo "no public key hash alias named $TZ_ALIAS"
- exit 1
-fi
-
-nonce_request=$(curl -s -X GET http://localhost:8080/vending-machine/$CHAIN_ID -w " HTTPSTATUS:%{http_code}")
-# nonce_request=$(curl -s -X GET http://$CLUSTER_ADDRESS/vending-machine/$CHAIN_ID -w " HTTPSTATUS:%{http_code}")
-body=$(echo $nonce_request | sed -e 's/ HTTPSTATUS\:.*//g')
-status=$(echo $nonce_request | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
-
-echo nonce_request $nonce_request
-echo status $status
-echo body $body ${#body}
-if [ "$status" -ne "200" ]
-then
-  echo "Error [HTTP status: $status]"
+if ! tezos-client show address $TZ_ALIAS >/dev/null 2>&1; then
+  echo "no public key hash alias named $TZ_ALIAS"
   exit 1
 fi
 
-SIGNATURE=$(tezos-client -p PsCARTHAGazK sign bytes 0x05${body} for ${TZ_ALIAS} | cut -f 2 -d " ")
+get_response_body() {
+  echo $1 | sed -e 's/ HTTPSTATUS\:.*//g'
+}
+get_response_status() {
+  printf '%q' $1 | sed -e 's/.*HTTPSTATUS://'
+}
+
+nonce_res=$(curl -s -X GET http://localhost:8080/vending-machine/$CHAIN_ID -w " HTTPSTATUS:%{http_code}")
+# nonce_res=$(curl -s -X GET http://$CLUSTER_ADDRESS/vending-machine/$CHAIN_ID -w " HTTPSTATUS:%{http_code}")
+NONCE=$(get_response_body "$nonce_res")
+nonce_res_status=$(get_response_status "$nonce_res")
+
+if [ "$nonce_res_status" != "200" ]; then
+  echo "Failed to get nonce. [HTTP status: $nonce_res_status]"
+  echo "$nonce_res"
+  exit 1
+fi
+
+echo NONCE: "$NONCE"
+
+SIGNATURE=$(tezos-client -p PsCARTHAGazK sign bytes 0x05${NONCE} for ${TZ_ALIAS} | cut -f 2 -d " ")
 PUBLIC_KEY=$(tezos-client show address ${TZ_ALIAS} 2>/dev/null | grep "Public Key: " | awk '{print $3}')
 
-echo $body | pbcopy
-echo signature $SIGNATURE
-echo PUBLIC_KEY $PUBLIC_KEY
+echo SIGNATURE: "$SIGNATURE"
+echo PUBLIC_KEY: "$PUBLIC_KEY"
 
-SECRET_URL_REQ=$(curl -s -X GET -d "nonce=${body}" -d "signature=${SIGNATURE}" -d "public_key=${PUBLIC_KEY}" http://localhost:8080/vending-machine)
-echo $SECRET_URL_REQ
-# SECRET_URL=$(curl -X POST -d "guid=${GUID}" -d "signature=${SIGNATURE}" -d "public_key=${PUBLIC_KEY}" https:/$CLUSTER_ADDRESS/vending-machine)
+secret_url_res=$(curl -s -X GET -d "nonce=${NONCE}" -d "signature=${SIGNATURE}" -d "public_key=${PUBLIC_KEY}" http://localhost:8080/vending-machine -w " HTTPSTATUS:%{http_code}")
+# https:/$CLUSTER_ADDRESS/vending-machine)
+SECRET_URL=$(get_response_body "$secret_url_res")
+secret_url_status=$(get_response_status "$secret_url_res")
 
-# echo $SIGNATURE
-# echo $PUBLIC_KEY
-# printf "URL: $SECRET_URL"
+echo SECRET_URL_STATUS: "$secret_url_status"
+
+if [ "$secret_url_status" != "200" ]; then
+  echo "Failed to get secret url. [HTTP status: $secret_url_status]"
+  echo "$secret_url_res"
+  exit 1
+fi
+
+echo SECRET_URL: "$SECRET_URL"
+
--- rpc-auth/server/index.py
+++ rpc-auth/server/index.py
@@ -14,8 +14,8 @@ from requests.models import HTTPError
 
 ## See if there is a better way to inject these variables
 CLUSTER_IP = "192.168.64.49"
-# CHAIN_ID = "NetXHFw7TkU5hhs"
-CHAIN_ID = None
+# CLUSTER_CHAIN_ID = "NetXHFw7TkU5hhs"
+CLUSTER_CHAIN_ID = None
 
 app = Flask(__name__)
 # redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
@@ -25,35 +25,39 @@ redis = Redis(host=CLUSTER_IP, port=6379)
 @app.route("/vending-machine/<chain_id>")
 def get_nonce(chain_id):
     try:
-      is_correct_chain_id = verify_chain_id(chain_id)
-      if not is_correct_chain_id:
-          return make_response("Unauthorized", 401)
+        is_correct_chain_id = verify_chain_id(chain_id)
+        if not is_correct_chain_id:
+            return "Unauthorized", 401
     except requests.exceptions.RequestException as e:
-      print("Failed to verify chain id", e)
-      return "Internal server error", 500
+        print("Failed to verify chain id.", e)
+        return "Internal server error", 500
 
     # tz1 = request.values.get("tz1")
     # if not tz1:
     #     return make_response("Missing tz1 address", 400)
 
     nonce = create_nonce()
+    # Set nonce with value for access count to 0
     redis.set(nonce, 0, ex=3)
     return nonce
 
 
 @app.route("/vending-machine")
 def get_cluster_url():
-    nonce, signature, pk = [
-        request.values[k] for k in ("nonce", "signature", "public_key")
-    ]
+    try:
+        nonce, signature, pk = [
+            request.values[k] for k in ("nonce", "signature", "public_key")
+        ]
+    except KeyError as e:
+        print("Request data:", request.values)
+        print(e)
+        return "Bad Request", 400
 
-    ### error handle values
-
-    nonce_access_count = int(redis.get(nonce))
-    if nonce_access_count != 0:
+    if not is_valid_nonce(nonce):
         return "Unauthorized", 401
 
     # Set nonce's access count to 1 so that it cannot be replayed
+    # (Will this actually prevent simultaneous requests with the same nonce??)
     redis.set(nonce, 1, xx=True, keepttl=True)
 
     is_valid_signature = verify_signature(pk, signature, nonce)
@@ -62,12 +66,19 @@ def get_cluster_url():
     return "VERIFIED"
 
 
-def verify_chain_id(chain_id):
-    global CHAIN_ID
+def is_valid_nonce(nonce):
+    nonce_from_redis = redis.get(nonce)
+    if nonce_from_redis != None and int(nonce_from_redis) == 0:
+        return True
+    return False
 
-    if not CHAIN_ID:
-        CHAIN_ID = get_chain_id()
-    if chain_id != CHAIN_ID:
+
+def verify_chain_id(chain_id):
+    global CLUSTER_CHAIN_ID
+
+    if not CLUSTER_CHAIN_ID:
+        CLUSTER_CHAIN_ID = get_chain_id()
+    if chain_id != CLUSTER_CHAIN_ID:
         return False
     return True
 
@@ -75,6 +86,7 @@ def verify_chain_id(chain_id):
 def get_chain_id():
     response = requests.get(f"http://{CLUSTER_IP}/chains/main/chain_id")
     return response.text.strip('\n\"')
+
 
 def create_nonce():
     return str(uuid4().hex)
@@ -85,7 +97,8 @@ def verify_signature(pk, signature, nonce):
         bytes_prefix = "0x05"
         Key.from_encoded_key(pk).verify(signature, bytes_prefix + nonce)
         return True
-    except ValueError:
+    except ValueError as e:
+        print("Error verifying signature:", e)
         return False
 
 
