77b724b39f4aec65e26cc18f6f27f0be316eaef9
Nicolas Ochem <nicolas.ochem@gmail.com>
2021-01-15 14:06:30 -0800

    support for multiple bakers
    Previously we used to create 4 accounts always. We simplify by
    creating just one account per baker. As a consequence, all blocks
    are baked on priority zero from the beginning.
    
    We add a new parameter to mkchain `--number-of-bakers`, defaults to
    1. If we pass a higher number n, it will create n bakers and n nodes
    baking for each baker.
    
    Bakers and nodes can be mapped using values.yaml: specify which node
    bakes for which address. This requires jq which requires a custom
    baking container. This also allowed me to remove helm hacks.
    
    We make a few assumptions for now:
    * every baking node is also a bootstrap node
    * the first baker's address is also the chain activator
    
    I tested in non-zerotier mode and zerotier mode with one create
    namespace and one invite namespace in the same minikube cluster; it
    works, but required me to remove the hard-coded nodePort for p2p.

--- key-importer/entrypoint.sh
+++ key-importer/entrypoint.sh
@@ -2,7 +2,10 @@
 mkdir -p /var/tezos/client
 chmod -R 777 /var/tezos/client
 
-for acct in ${BOOTSTRAP_ACCOUNTS}; do
-    key=$(eval echo \$${acct}_${KEYS_TYPE}_key)
-    tezos-client -d /var/tezos/client --protocol PsDELPH1Kxsx import ${KEYS_TYPE} key $acct unencrypted:${key} -f
+echo $ACCOUNTS | jq -c --raw-output .[] | while read line; do
+    key=$(echo $line | jq -r '.key')
+    name=$(echo $line | jq -r '.name')
+    keytype=$(echo $line | jq -r '.type')
+    printf "\nImporting key ${name}\n"
+    tezos-client -d /var/tezos/client --protocol PsDELPH1Kxsx import ${keytype} key ${name} unencrypted:${key} -f
 done
