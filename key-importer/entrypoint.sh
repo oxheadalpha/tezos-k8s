384e2f860ae430903831c9f9477ca8c1d960fcb2
Roland C. Dowdeswell <elric@imrryr.org>
2021-01-21 21:20:38 +0000

    Only import the required keys in init containers
    We do import all of the keys into the activate-job, because it needs
    to know about all of the accounts in order to set them up with a init
    balance.
    
    We also stop generating /etc/tezos/parameters.json except on our
    activate-job because that's the only place where it is necessary.

--- key-importer/entrypoint.sh
+++ key-importer/entrypoint.sh
@@ -1,12 +1,52 @@
 #!/bin/sh
+
 mkdir -p /var/tezos/client
 chmod -R 777 /var/tezos/client
 
-echo $ACCOUNTS | jq -c --raw-output .[] | while read line; do
-    key=$(echo $line | jq -r '.key')
-    name=$(echo $line | jq -r '.name')
-    keytype=$(echo $line | jq -r '.type')
-    protocol=$(echo $CHAIN_PARAMS | jq -r '.protocol_hash')
-    printf "\nImporting key ${name}\n"
-    tezos-client -d /var/tezos/client --protocol ${protocol} import ${keytype} key ${name} unencrypted:${key} -f
-done
+PROTOCOL=$(echo $CHAIN_PARAMS | jq -r '.protocol_hash')
+
+import_single_key() {
+    read keytype
+    read name
+    read key
+
+echo tezos-client -d /var/tezos/client --protocol $PROTOCOL \
+	import ${keytype} key ${name} unencrypted:${key} -f
+
+    tezos-client -d /var/tezos/client --protocol $PROTOCOL \
+	import ${keytype} key ${name} unencrypted:${key} -f
+    echo "Imported keytype $keytype name $name"
+}
+
+import_all_keys() {
+    echo $ACCOUNTS | jq -c --raw-output .[] | while read LINE; do
+	echo $LINE | jq -r '.type, .name, .key' | import_single_key
+    done
+}
+
+import_key() {
+    echo $ACCOUNTS | \
+    jq -r ".[] | select(.name == \"baker$1\") | .type, .name, .key" | 
+	import_single_key
+}
+
+HOSTNAME=$(hostname)
+MY_NODE=${HOSTNAME##tezos-baking-node-}
+
+#
+# The activation job needs all of the keys:
+
+if [ "${HOSTNAME#activate-job}" != ${HOSTNAME} ]; then
+    import_all_keys
+    exit 0
+fi
+
+#
+# For some reason, we all need to have baker0's key.  We can likely
+# fix this in the future, I would imagine.
+
+import_key 0
+
+if [ "$MY_NODE" != "$HOSTNAME" -a "$MY_NODE" != 0 ]; then
+    import_key $MY_NODE
+fi
