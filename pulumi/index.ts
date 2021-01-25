8f6c4eeb0c7ae958b4400f11d5ea5a160d76abc1
Roland C. Dowdeswell <elric@imrryr.org>
2021-01-25 09:38:11 -0800

    Update and simplify pulumi image locations, generation code
    We update the images names to those used at the head of the current
    master branch.  We also start to use a small loop to generate the
    locations of the docker images, this reduces repetition.
    
    pulumi should also use the tezos namespace.
    
    Choose the size of the cluster based on the number of tezos nodes.
    
    pulumi/index.ts: make rpc-auth and nginx conditional.
    
    Add a README.md to the pulumi directory.
    
    Choose the CHAIN_NAME from the env var rather than hard-coded.
    
    Put out chain name into the objects that we create.
    
    General cleanup.

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -2,62 +2,93 @@ import * as pulumi from "@pulumi/pulumi";
 import * as eks from "@pulumi/eks";
 import * as k8s from "@pulumi/kubernetes";
 import * as awsx from "@pulumi/awsx";
-import * as aws from "@pulumi/aws";
-
-// Create a repository
-const repo = new awsx.ecr.Repository("tezos-k8s");
 
 import * as fs from 'fs';
 import * as YAML from 'yaml'
 
-const { readdirSync } = require('fs')
+const repo = new awsx.ecr.Repository("tezos-k8s");
 
 // Manual step: create a pulumi_values.yaml in the top level dir
 // with mkchain command:
 //   mkchain pulumi
 //
-const helm_values_file = fs.readFileSync('../pulumi_values.yaml', 'utf8')
-const helm_values = YAML.parse(helm_values_file)
 
-const nginx_ingress_helm_values_file = fs.readFileSync('nginx_ingress_values.yaml', 'utf8')
-const nginx_ingress_helm_values = YAML.parse(nginx_ingress_helm_values_file)
+const chainName = process.env.CHAIN_NAME || "pulumi";
 
-helm_values["container_images"]["zerotier_docker_image"] = repo.buildAndPushImage("../zerotier");
-helm_values["container_images"]["tezos_chain_initiator_docker_image"] = repo.buildAndPushImage("../chain-initiator");
-helm_values["container_images"]["tezos_config_generator_docker_image"] = repo.buildAndPushImage("../config-generator");
-helm_values["container_images"]["tezos_key_importer_docker_image"] = repo.buildAndPushImage("../key-importer");
+const defaultHelmValuesFile = fs.readFileSync("../charts/tezos/values.yaml", 'utf8')
+const defaultHelmValues = YAML.parse(defaultHelmValuesFile)
 
-const rpc_auth_helm_values = { "container_images" : { "rpc_auth_image": repo.buildAndPushImage("../rpc-auth") } };
+const helmValuesFile = fs.readFileSync(chainName + '_values.yaml', 'utf8')
+const helmValues = YAML.parse(helmValuesFile)
 
-const vpc = new awsx.ec2.Vpc("tezos-vpc", {});
+const tezosK8sImages = defaultHelmValues["tezos_k8s_images"]
+const pulumiTaggedImages = Object.entries(tezosK8sImages).reduce(
+    (obj, [key]) => {
+	obj[key] = repo.buildAndPushImage(`../${key.replace(/_/g, "-")}`)
+	return obj
+    },
+    {}
+)
+
+helmValues["tezos_k8s_images"] = pulumiTaggedImages
+
+const vpc = new awsx.ec2.Vpc(chainName + "-vpc", {});
+
+// For now, we set the number of EKS nodes to be the number of
+// Tezos bakers/nodes divided by 12.  We should fix this to
+// be a little more automagic.  Why 12?  It's a guess currently,
+// we should experiment a little to get a better number.  We add
+// three because that's the minimum.
+
+const numBakers = helmValues.nodes?.baking?.length || 0
+const numRegularNodes  = helmValues.nodes?.regular?.length || 0
+
+const totalTezosNodes = numBakers + numRegularNodes
+const desiredClusterCapacity = Math.round(totalTezosNodes / 12 + 3);
 
 // Create an EKS cluster.
-const cluster = new eks.Cluster("tq-private-chain", {
+const cluster = new eks.Cluster(chainName + "-chain", {
     vpcId: vpc.id,
     subnetIds: vpc.publicSubnetIds,
+    instanceType: "t3.xlarge",
+    desiredCapacity: desiredClusterCapacity,
+    minSize: 3,
+    maxSize: 100,
 })
+
+const ns = new k8s.core.v1.Namespace("tezos", {metadata: {name:"tezos",}},
+					      { provider: cluster.provider});
+export const nsName = ns.metadata.name;
 
 // Deploy Tezos into our cluster.
 const chain = new k8s.helm.v2.Chart("chain", {
+    namespace: nsName,
     path: "../charts/tezos",
-    values: helm_values,
+    values: helmValues,
 }, { providers: { "kubernetes": cluster.provider } });
 
-const rpc = new k8s.helm.v2.Chart("rpc-auth", {
-    path: "../charts/rpc-auth",
-    values: rpc_auth_helm_values,
-}, { providers: { "kubernetes": cluster.provider } });
+if (helmValues["rpc_auth"] == true) {
+    const nginxIngressHelmValues_file
+	= fs.readFileSync('nginx_ingress_values.yaml', 'utf8')
+    const nginxIngressHelmValues = YAML.parse(nginxIngressHelmValues_file)
 
-// Manual step at this point:
-// * create a certificate
-// * put certificate arn in the nginx_ingress_values.yaml
+    const rpc = new k8s.helm.v2.Chart("rpc-auth", {
+	namespace: nsName,
+	path: "../charts/rpc-auth",
+	values: helmValues,
+    }, { providers: { "kubernetes": cluster.provider } });
 
-const nginxIngress = new k8s.helm.v2.Chart("nginx-ingress", {
-    chart: "ingress-nginx",
-    fetchOpts: {
-      repo: "https://kubernetes.github.io/ingress-nginx" },
-    values: nginx_ingress_helm_values,
-}, { providers: { "kubernetes": cluster.provider } });
+    // Manual step at this point:
+    // * create a certificate
+    // * put certificate arn in the nginx_ingress_values.yaml
+    const nginxIngress = new k8s.helm.v2.Chart("nginx-ingress", {
+	namespace: nsName,
+	chart: "ingress-nginx",
+	fetchOpts: {
+	  repo: "https://kubernetes.github.io/ingress-nginx" },
+	values: nginxIngressHelmValues,
+    }, { providers: { "kubernetes": cluster.provider } });
+}
 
 // Manual steps after all is done:
 // Enable proxy protocol v2 on the target groups:
