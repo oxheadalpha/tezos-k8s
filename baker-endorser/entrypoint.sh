e21fe77b8551a22b38724bbd46cab893fe8cd886
Nicolas Ochem <nicolas.ochem@gmail.com>
2021-01-26 16:35:21 -0800

    more explicit var name per review

--- baker-endorser/entrypoint.sh
+++ baker-endorser/entrypoint.sh
@@ -3,9 +3,9 @@
 set -x
 
 proto_command=$(echo $CHAIN_PARAMS | jq -r '.proto_command')
-if [ "${WHAT}" == "baker" ]; then
+if [ "${DAEMON}" == "baker" ]; then
     extra_args="with local node /var/tezos/node"
 fi
 POD_INDEX=$(echo $POD_NAME | sed -e s/tezos-baking-node-//)
 baker_account=$(echo $NODES | jq -r ".baking[${POD_INDEX}].bake_for")
-/usr/local/bin/tezos-${WHAT}-${proto_command} -d /var/tezos/client run ${extra_args} ${baker_account:?Error: baker account not set}
+/usr/local/bin/tezos-${DAEMON}-${proto_command} -d /var/tezos/client run ${extra_args} ${baker_account:?Error: baker account not set}
