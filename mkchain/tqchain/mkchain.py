f4aa12efb0b96e059e52a6e9ebecd52166878d58
Roland C. Dowdeswell <elric@imrryr.org>
2021-02-08 22:38:15 +0000

    Decouple number-of-nodes from number-of-bakers (#100)
    * Decouple number-of-nodes from number-of-bakers
    
    We now consider --number-of-nodes to be the nodes which do not
    include a baker/endorser pair.  It's more natural to consider
    that:
    
        $ mkchain --number-of-bakers 40 --number-of-nodes 5 my_chain
    
    will bring up 40 baker nodes and 5 plain nodes.
    
    This is helpful with pulumi because it has been waiting for zero nodes
    to come up and timing out.  With the current code:
    
        $ mkchain --number-of-bakers 40 my_chain
    
    will bring up 40 bakers and a single node.
    
    * Allow either zero bakers or nodes but not both.
    
    * Update docs
    
    * mkchain/README.md: Improve readability
    
    * Doc updates
    
    Co-authored-by: Aryeh Harris <harryttd@gmail.com>

--- mkchain/tqchain/mkchain.py
+++ mkchain/tqchain/mkchain.py
@@ -120,15 +120,25 @@ def pull_docker_images(images):
 def main():
     args = get_args()
 
-    if args.number_of_nodes < 1:
+    if args.number_of_nodes < 0:
         print(
-            f"Invalid argument --number-of-nodes {args.number_of_nodes}, must be 1 or more"
+            f"Invalid argument --number-of-nodes ({args.number_of_nodes}) "
+            f"must be non-negative"
         )
         exit(1)
 
-    if args.number_of_bakers < 1:
+    if args.number_of_bakers < 0:
         print(
-            f"Invalid argument --number-of-bakers {args.number_of_bakers}, must be 1 or more"
+            f"Invalid argument --number-of-bakers ({args.number_of_bakers}) "
+            f"must be non-negative"
+        )
+        exit(1)
+
+    if args.number_of_nodes + args.number_of_bakers < 1:
+        print(
+            f"Invalid arguments: either "
+            f"--number-of-nodes ({args.number_of_nodes}) or "
+            f"--number-of-bakers ({args.number_of_bakers}) must be non-zero"
         )
         exit(1)
 
@@ -195,7 +205,7 @@ def main():
 
     creation_nodes = {
         "baking": [{"bake_for": f"baker{n}"} for n in range(args.number_of_bakers)],
-        "regular": [{} for n in range(args.number_of_nodes - args.number_of_bakers)],
+        "regular": [{} for n in range(args.number_of_nodes)],
     }
 
     # first nodes are acting as bootstrap nodes for the others
