ea4730c81ebf2f50cb33be57b9ff2070ec33898b
Nicolas Ochem <nicolas.ochem@gmail.com>
2021-03-02 11:13:50 -0800

    add NLB and switch to florence branch

--- config-generator/entrypoint.py
+++ config-generator/entrypoint.py
@@ -70,6 +70,7 @@ def main():
             bootstrap_peers.extend(local_bootstrap_peers)
             if not bootstrap_peers:
                 bootstrap_peers = [f"tezos-baking-node-0.tezos-baking-node:9732"]
+        #net_addr="52.15.225.122"
 
         config_json = json.dumps(
             create_node_config_json(
