5ce532e270dc904c57b46e1a80bdae7fda95d4fd
Aryeh Harris <harryttd@gmail.com>
2021-03-10 14:06:15 -0500

    Update chain-initiator

--- chain-initiator/entrypoint.sh
+++ chain-initiator/entrypoint.sh
@@ -7,5 +7,5 @@ until nslookup tezos-node-rpc; do echo waiting for tezos-node-rpc; sleep 2; done
 until wget -O- http://tezos-node-rpc:8732/version; do sleep 2; done;
 
 protocol_hash=$(echo $CHAIN_PARAMS | jq -r '.protocol_hash')
-activation_account=$(echo $CHAIN_PARAMS | jq -r '.activation_account')
+activation_account=$(echo $CHAIN_PARAMS | jq -r '.network.activation_account_name')
 /usr/local/bin/tezos-client -A tezos-node-rpc -P 8732 -d /var/tezos/client -l --block genesis activate protocol "${protocol_hash}" with fitness -1 and key "${activation_account}" and parameters /etc/tezos/parameters.json
