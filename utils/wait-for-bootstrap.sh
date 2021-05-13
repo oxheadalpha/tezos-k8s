19b4458709d48d6fc7f5847d86649b4be6c3778d
Nicolas Ochem <nicolas.ochem@gmail.com>
2021-05-13 16:25:16 -0700

    undo bootstrap changes

--- utils/wait-for-bootstrap.sh
+++ utils/wait-for-bootstrap.sh
@@ -10,26 +10,20 @@ if [ -s /var/tezos/node/peers.json ] && [ "$(jq length /var/tezos/node/peers.jso
     exit 0
 fi
 
-BAKING_BOOTSTRAP_NODES=$(
+BOOTSTRAP_NODES=$(
 	echo "$NODES" | \
 	    jq -r '[.[]|to_entries]|flatten[]
 		  |select(.value.is_bootstrap_node)
 		  |.key + "." + (.key|sub("-[\\d]+$"; ""))'
 )
-REGULAR_BOOTSTRAP_NODES=$(
-	echo "$NODES" | \
-	    jq -r '.regular|to_entries[]
-		  |select(.value.is_bootstrap_node)
-		  |.key+".tezos-node"'
-)
 
-if [ -z "$BAKING_BOOTSTRAP_NODES" ] && [ -z "$REGULAR_BOOTSTRAP_NODES" ]; then
+if [ -z "$BOOTSTRAP_NODES" ]; then
     echo No bootstrap nodes were provided
     exit 1
 fi
 
 HOST=$(hostname -f)
-for node in $BAKING_BOOTSTRAP_NODES $REGULAR_BOOTSTRAP_NODES; do
+for node in $BOOTSTRAP_NODES; do
     if [ "${HOST##$node}" != "$HOST" ]; then
 	echo "I am $node!"
 	echo "I'm the one of the bootstrap nodes: do not wait for myself"
@@ -67,7 +61,7 @@ randomsleep() {
 echo "waiting for bootstrap nodes to accept connections"
 
 while :; do
-    for node in $BAKING_BOOTSTRAP_NODES $REGULAR_BOOTSTRAP_NODES; do
+    for node in $BOOTSTRAP_NODES; do
 	if </dev/null nc -q 0 ${node} 8732; then
 	    echo "$node is up"
 	    echo "Found bootstrap node, exiting"
