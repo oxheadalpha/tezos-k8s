5406d1430a0cd0595d0684a9d70f5fc76678492d
Roland C. Dowdeswell <elric@imrryr.org>
2021-07-01 17:28:37 +0100

    DRAFT: optionally use tezedge

--- utils/config-generator.sh
+++ utils/config-generator.sh
@@ -11,6 +11,51 @@ set -e
 python3 /config-generator.py "$@"
 set +e
 
+cat > /etc/tezos/tezedge.conf <<EOM
+--network=sandbox
+--p2p-port=9732
+--rpc-port=8732
+--websocket-address=0.0.0.0:4927
+--init-sapling-spend-params-file=/sapling-spend.params
+--init-sapling-output-params-file=/sapling-output.params
+--tezos-data-dir=/var/tezos/node/data
+--bootstrap-db-path=/var/tezos/node/bootstrap
+
+--bootstrap-lookup-address=tezos-baking-node-0.tezos-baking-node
+
+--sandbox-patch-context-json-file=/etc/tezos/genesis.json
+--identity-file=/tmp/tezedge/identity.json
+--identity-expected-pow=0
+--log-format=simple
+--log-level=info
+--ocaml-log-enabled=false
+--peer-thresh-low=10
+--peer-thresh-high=15
+--protocol-runner=/protocol-runner
+--ffi-pool-max-connections=10
+--ffi-trpap-pool-max-connections=10
+--ffi-twcap-pool-max-connections=10
+--ffi-pool-connection-timeout-in-secs=60
+--ffi-trpap-pool-connection-timeout-in-secs=60
+--ffi-twcap-pool-connection-timeout-in-secs=60
+--ffi-pool-max-lifetime-in-secs=21600
+--ffi-trpap-pool-max-lifetime-in-secs=21600
+--ffi-twcap-pool-max-lifetime-in-secs=21600
+--ffi-pool-idle-timeout-in-secs=1800
+--ffi-trpap-pool-idle-timeout-in-secs=1800
+--ffi-twcap-pool-idle-timeout-in-secs=1800
+--actions-store-backend=rocksdb
+--compute-context-action-tree-hashes=false
+--tokio-threads=0
+--enable-testchain=false
+EOM
+
+cat > /etc/tezos/genesis.json <<EOM
+{
+  "genesis_pubkey": "BKn8byUN52nWQH5ETTudWM62WzAtuz2pkKBXf2eN8XrjtmkEUyt"
+}
+EOM
+
 #
 # Next we write the current baker ccount into /etc/tezos/baking-account.
 # We do it here because we shall use jq to process some of the environment
