96f30ee9edbb04de5e5a7ad017706181be6ff12d
Nicolas Ochem <nicolas.ochem@gmail.com>
2021-07-16 17:44:38 -0700

    fix #227 handle the new ACL setting
    To run octez master branch, it's necessary to put the following setting
    in values.yaml
    
    ```
    open_acls: true
    ```
    
    This will configure the baking nodes as follows:
    
    ```
      "rpc": {
        "listen-addrs": [
          "172.17.0.7:8732",
          "127.0.0.1:8732"
        ],
        "acl": [
          {
            "address": "172.17.0.7",
            "blacklist": []
          }
        ]
    ```
    
    The above configuration is needed for chain activation, since the
    activation operation uses RPCs which are blacklisted by default.
    
    The ACL setting needs to be set with the listen addr, so it's not
    possible to pass it as a node config with values.yaml. It has to be
    generated on the fly. We pass the flag to config-generator.py which adds
    the setting.
    
    When set to `true`, this toggle will break octez v9/10 since it will not
    recognize an `acl` object in `config/rpc`.
    
    The toggle will go away at some point (when v9/v10 is no longer used)

--- utils/config-generator.py
+++ utils/config-generator.py
@@ -18,6 +18,7 @@ ACCOUNTS = json.loads(os.environ["ACCOUNTS"])
 CHAIN_PARAMS = json.loads(os.environ["CHAIN_PARAMS"])
 NODES = json.loads(os.environ["NODES"])
 SIGNERS = json.loads(os.environ["SIGNERS"])
+OPEN_ACLS = os.environ["OPEN_ACLS"]
 
 MY_POD_NAME = os.environ["MY_POD_NAME"]
 MY_POD_TYPE = os.environ["MY_POD_TYPE"]
@@ -503,6 +504,8 @@ def create_node_config_json(
         },
         # "log": {"level": "debug"},
     }
+    if OPEN_ACLS == "true":
+        computed_node_config["rpc"]["acl"] = [ { "address": os.getenv('MY_POD_IP'), "blacklist": [] } ]
     node_config = recursive_update(values_node_config, computed_node_config)
 
     if THIS_IS_A_PUBLIC_NET:
