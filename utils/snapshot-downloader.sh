1b1f64b0c98914ae45f7294f2188048b0a9d9e99
orcutt989 <jimmy.orcutt@tqtezos.com>
2021-09-13 19:59:13 -0400

    add new image

--- utils/snapshot-downloader.sh
+++ utils/snapshot-downloader.sh
@@ -15,9 +15,9 @@ echo My nodes history mode: $my_nodes_history_mode
 case "$my_nodes_history_mode" in
         full)           snapshot_url="$FULL_SNAPSHOT_URL"       ;;
         rolling)        snapshot_url="$ROLLING_SNAPSHOT_URL"    ;;
-        *)              echo "No snapshot URL provide for node, exiting"
-                        exit 1;;
 esac
+
+curl https://tezos-snapshots.s3-accelerate.amazonaws.com/vol-00e24ee0c708bcce9/2021-09-13T16%3A58%3A16%2B00%3A00+snap-0074628953cabfaae+-+grenadanet-archive-snapshot.tar.lz4 | lz4 -d | tar -x -C /var/tezos
 
 if [ ! -d $node_data_dir/context ]; then
 	echo "Did not find pre-existing data, importing blockchain"
