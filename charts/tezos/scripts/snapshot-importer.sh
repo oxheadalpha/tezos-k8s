2280052c377c3922c2984b5e78517350b2fd0ba0
Aryeh Harris <harryttd@gmail.com>
2021-11-18 15:39:11 -0500

    Merge tar & snapshot download into one script + handle multi tar urls

--- charts/tezos/scripts/snapshot-importer.sh
+++ charts/tezos/scripts/snapshot-importer.sh
@@ -1,7 +1,6 @@
 set -ex
 
 bin_dir="/usr/local/bin"
-
 data_dir="/var/tezos"
 node_dir="$data_dir/node"
 node_data_dir="$node_dir/data"
@@ -9,12 +8,16 @@ node="$bin_dir/tezos-node"
 snapshot_file=${node_dir}/chain.snapshot
 
 if [ -d ${node_data_dir}/context ]; then
-    echo "Blockchain has already been imported, exiting"
+    echo "Blockchain has already been imported. If a tarball"
+    echo "instead of a regular tezos snapshot was used, it was"
+    echo "imported in the snapshot-downloader container."
     exit 0
 fi
 
 cp -v /etc/tezos/config.json ${node_data_dir}
+
 ${node} snapshot import ${snapshot_file} --data-dir ${node_data_dir} \
     --network $CHAIN_NAME
 find ${node_dir}
+
 rm -rvf ${snapshot_file}
