ea0a9e32c61e86b886c1faee3ebd22e56e45c114
Nicolas Ochem <nicolas.ochem@gmail.com>
2021-11-23 08:33:10 -0800

    add an optional fitness parameter to activation

--- charts/tezos/scripts/chain-initiator.sh
+++ charts/tezos/scripts/chain-initiator.sh
@@ -11,10 +11,18 @@ if ! $CLIENT rpc get /chains/main/blocks/head/header | grep '"level": 0,'; then
     exit 0
 fi
 
+activation_fitness_param="{{ .Values.activation.fitness }}"
+if [  -z "$activation_fitness_param" ]; then
+    FITNESS=$activation_fitness_param
+else
+    FITNESS="-1"
+fi
+
+
 echo Activating chain:
 $CLIENT -d /var/tezos/client --block					\
 	genesis activate protocol					\
 	{{ .Values.activation.protocol_hash }}				\
-	with fitness -1 and key						\
+	with fitness ${FITNESS} and key					\
 	$( cat /etc/tezos/activation_account_name )			\
 	and parameters /etc/tezos/parameters.json 2>&1 | head -200
