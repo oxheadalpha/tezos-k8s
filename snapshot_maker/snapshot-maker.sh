eb11f486a3e2fd049470c89b0585dae47bc046b8
orcutt989 <jimmy.orcutt@tqtezos.com>
2022-02-01 10:40:52 -0500

    fix snapshot name

--- snapshot_maker/snapshot-maker.sh
+++ snapshot_maker/snapshot-maker.sh
@@ -45,19 +45,13 @@ done
 NAMESPACE="${NAMESPACE}" yq e -i '.metadata.namespace=strenv(NAMESPACE)' createVolumeSnapshot.yaml
 
 # EBS Snapshot name based on current time and date
-SNAPSHOT_NAME=$(date "+%Y-%m-%d-%H-%M-%S" "$@")-node-snapshot
+SNAPSHOT_NAME=$(date "+%Y-%m-%d-%H-%M-%S" "$@")-"${HISTORY_MODE}"-node-snapshot
 
 # Update volume snapshot name
 SNAPSHOT_NAME="${SNAPSHOT_NAME}" yq e -i '.metadata.name=strenv(SNAPSHOT_NAME)' createVolumeSnapshot.yaml
 
-# hangzhounet-shots PVC is called 'var-volume-archive-node' for some reason
-HISTORY_MODE=$(kubectl get pods -n "${NAMESAPCE}" -l appType=tezos-node -o jsonpath="{.items[0].metadata.labels.node_class_history_mode}")
-if [ "$HISTORY_MODE" ]; then
-    PERSISTENT_VOLUME_CLAIM=var-volume-snapshot-"${HISTORY_MODE}"-node-0
-else
-    PERSISTENT_VOLUME_CLAIM=var-volume-tezos-node-0
-fi
 
+PERSISTENT_VOLUME_CLAIM=var-volume-snapshot-"${HISTORY_MODE}"-node-0
 PERSISTENT_VOLUME_CLAIM="${PERSISTENT_VOLUME_CLAIM}" yq e -i '.spec.source.persistentVolumeClaimName=strenv(PERSISTENT_VOLUME_CLAIM)' createVolumeSnapshot.yaml
 
 # Create snapshot
@@ -102,7 +96,8 @@ fi
 NAMESPACE="${NAMESPACE}" yq e -i '.metadata.namespace=strenv(NAMESPACE)' volumeFromSnap.yaml
 
 ## Snapshot volume name
-VOLUME_NAME="${NAMESPACE}-snap-volume" yq e -i '.metadata.name=strenv(VOLUME_NAME)' volumeFromSnap.yaml
+VOLUME_NAME="${NAMESPACE}-snap-volume"
+VOLUME_NAME="${VOLUME_NAME}" yq e -i '.metadata.name=strenv(VOLUME_NAME)' volumeFromSnap.yaml
 
 if ! kubectl apply -f volumeFromSnap.yaml
 then
@@ -114,7 +109,7 @@ fi
 printf "%s PersistentVolumeClaim ${NAMESPACE}-snap-volume created successfully in namespace ${NAMESPACE}.\n" "$(date "+%Y-%m-%d %H:%M:%S" "$@")"
 
 # Set new PVC Name in snapshotting job
-VOLUME_NAME="${NAMESPACE}-snap-volume" yq e -i '.spec.template.spec.volumes[0].persistentVolumeClaim.claimName=strenv(VOLUME_NAME)' mainJob.yaml
+VOLUME_NAME="${VOLUME_NAME}" yq e -i '.spec.template.spec.volumes[0].persistentVolumeClaim.claimName=strenv(VOLUME_NAME)' mainJob.yaml
 
 # Set image name for zip and upload
 IMAGE_NAME="${IMAGE_NAME}" yq e -i '.spec.template.spec.containers[1].image=strenv(IMAGE_NAME)' mainJob.yaml
