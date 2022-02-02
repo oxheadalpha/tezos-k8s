b2f80a35677980f4f25c645368b39f8a777d15c3
orcutt989 <jimmy.orcutt@tqtezos.com>
2022-02-02 16:24:28 -0500

    add debug

--- snapshot_maker/snapshot-maker.sh
+++ snapshot_maker/snapshot-maker.sh
@@ -156,7 +156,8 @@ then
     exit 1
 fi
 
-sleep 2
+printf "%s Sleeping and waiting for job to create.\n" "$(date "+%Y-%m-%d %H:%M:%S" "$@")"
+sleep 10
 
 # Wait for snapshotting job to complete
 while [ "$(kubectl get jobs "${ZIP_AND_UPLOAD_JOB_NAME}" --namespace "${NAMESPACE}" -o jsonpath='{.status.conditions[?(@.type=="Complete")].status}')" != "True" ]; do
