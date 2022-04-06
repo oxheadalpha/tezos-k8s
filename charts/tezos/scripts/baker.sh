2e5c00476141bbeaeaee92f4b6474083ef5c105e
Nicolas Ochem <nicolas.ochem@gmail.com>
2022-04-06 09:57:47 -0700

    switch to ithaca as default protocol for priv chains
    * mkchain produces values.yaml for ithaca chain
    * example in values.yaml comments is now ithaca
    * remove any trace of the endorser in our code

--- charts/tezos/scripts/baker.sh
+++ charts/tezos/scripts/baker.sh
@@ -8,14 +8,10 @@ NODE_DATA_DIR="$TEZ_VAR/node/data"
 
 proto_command="{{ .command_in_tpl }}"
 
-if [ "${DAEMON}" == "baker" ]; then
-    extra_args="with local node $NODE_DATA_DIR"
-fi
-
 my_baker_account="$(cat /etc/tezos/baker-account )"
 
 CLIENT="$TEZ_BIN/tezos-client -d $CLIENT_DIR"
-CMD="$TEZ_BIN/tezos-$DAEMON-$proto_command -d $CLIENT_DIR"
+CMD="$TEZ_BIN/tezos-baker-$proto_command -d $CLIENT_DIR"
 
 # ensure we can run tezos-client commands without specifying client dir
 ln -s /var/tezos/client /home/tezos/.tezos-client
@@ -24,4 +20,4 @@ while ! $CLIENT rpc get chains/main/blocks/head; do
     sleep 5
 done
 
-exec $CMD run ${extra_args} ${my_baker_account}
+exec $CMD run with local node $NODE_DATA_DIR ${my_baker_account}
