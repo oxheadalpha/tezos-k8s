687f65e0f38e3618e099cc0f3371ada8db0f8dd4
orcutt989 <jimmy.orcutt@tqtezos.com>
2022-04-20 12:59:28 -0400

    restore tutorial files

--- docusaurus/docs/index.md
+++
@@ -1,5 +0,0 @@
----
-sidebar_position: 1
----
-
-# tezos-k8s documentation
--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -6,15 +6,15 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'tezos-k8s',
-  tagline: 'Deploy a Tezos Blockchain on Kubernetes',
-  url: 'http://www.oxheadalpha.com/tezos-k8s',
+  title: 'My Site',
+  tagline: 'Dinosaurs are cool',
+  url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'oxheadalpha', // Usually your GitHub org/user name.
-  projectName: 'tezos-k8s', // Usually your repo name.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
 
   presets: [
     [
