c6629a93bab615c7925b35c4f0c2dd5b38b6a03c
orcutt989 <jimmy.orcutt@tqtezos.com>
2022-04-20 13:02:10 -0400

    fix baseurl

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  baseUrl: '/tezos-k8s/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
