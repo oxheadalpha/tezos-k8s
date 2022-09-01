b20365a6c545c510980c955db958a414a5036f28
orcutt989 <orcutt989@gmail.com>
2022-09-01 19:32:12 -0400

    try sleep

--- snapshotEngine/zip-and-upload.sh
+++ snapshotEngine/zip-and-upload.sh
@@ -148,46 +148,48 @@ if [ "${HISTORY_MODE}" = rolling ]; then
     ROLLING_TARBALL_FILENAME=tezos-"${NETWORK}"-rolling-tarball-"${BLOCK_HEIGHT}".lz4
     IMPORT_IN_PROGRESS=/rolling-tarball-restore/snapshot-import-in-progress
 
-    # Wait for rolling snapshot file
-    while  ! [ -f "${ROLLING_SNAPSHOT}" ]; do
-        printf "%s Waiting for ${ROLLING_SNAPSHOT} to exist...\n" "$(date "+%Y-%m-%d %H:%M:%S" "$@")"
-        
-        if [ "${HISTORY_MODE}" = archive ]; then
-            sleep 15m
-        else
-            sleep 2m
-        fi
-    done
-
-    # Wait for rolling snapshot to import to temporary filesystem for tarball.
-    while  [ -f "${IMPORT_IN_PROGRESS}" ]; do
-        printf "%s Waiting for snapshot to import...\n" "$(date "+%Y-%m-%d %H:%M:%S" "$@")"
-        if [ "${HISTORY_MODE}" = archive ]; then
-            sleep 15m
-        else
-            sleep 2m
-        fi
-    done
-
     # # Wait for rolling snapshot file
-    # until [ -f "${ROLLING_SNAPSHOT}" ]; do
+    # while  ! [ -f "${ROLLING_SNAPSHOT}" ]; do
     #     printf "%s Waiting for ${ROLLING_SNAPSHOT} to exist...\n" "$(date "+%Y-%m-%d %H:%M:%S" "$@")"
-    #     until [ -f "${ROLLING_SNAPSHOT}" ]; do
-    #         if [ -f "${ROLLING_SNAPSHOT}" ];then
-    #             break
-    #         fi
-    #     done
+        
+    #     if [ "${HISTORY_MODE}" = archive ]; then
+    #         sleep 15m
+    #     else
+    #         sleep 2m
+    #     fi
     # done
 
     # # Wait for rolling snapshot to import to temporary filesystem for tarball.
     # while  [ -f "${IMPORT_IN_PROGRESS}" ]; do
     #     printf "%s Waiting for snapshot to import...\n" "$(date "+%Y-%m-%d %H:%M:%S" "$@")"
-    #     while  [ -f "${IMPORT_IN_PROGRESS}" ]; do
-    #         if ! [ -f "${IMPORT_IN_PROGRESS}" ]; then
-    #             break
-    #         fi
-    #     done
+    #     if [ "${HISTORY_MODE}" = archive ]; then
+    #         sleep 15m
+    #     else
+    #         sleep 2m
+    #     fi
     # done
+
+    # Wait for rolling snapshot file
+    until [ -f "${ROLLING_SNAPSHOT}" ]; do
+        printf "%s Waiting for ${ROLLING_SNAPSHOT} to exist...\n" "$(date "+%Y-%m-%d %H:%M:%S" "$@")"
+        until [ -f "${ROLLING_SNAPSHOT}" ]; do
+            if [ -f "${ROLLING_SNAPSHOT}" ];then
+                break
+            fi
+        done
+    done
+
+    sleep 10s
+
+    # Wait for rolling snapshot to import to temporary filesystem for tarball.
+    while  [ -f "${IMPORT_IN_PROGRESS}" ]; do
+        printf "%s Waiting for snapshot to import...\n" "$(date "+%Y-%m-%d %H:%M:%S" "$@")"
+        while  [ -f "${IMPORT_IN_PROGRESS}" ]; do
+            if ! [ -f "${IMPORT_IN_PROGRESS}" ]; then
+                break
+            fi
+        done
+    done
 
     # LZ4 /"${HISTORY_MODE}"-snapshot-cache-volume/var/tezos/node selectively and upload to S3
     printf "%s ********************* Rolling Tarball *********************\\n" "$(date "+%Y-%m-%d %H:%M:%S" "$@")"
