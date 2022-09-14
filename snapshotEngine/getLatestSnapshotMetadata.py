dc3c29de2f4376057ef32d4c809f0543695e4150
orcutt989 <orcutt989@gmail.com>
2022-09-14 14:57:47 -0400

    only serve artifacts for the newest octez per network

--- snapshotEngine/getLatestSnapshotMetadata.py
+++ snapshotEngine/getLatestSnapshotMetadata.py
@@ -2,6 +2,9 @@ from genericpath import exists
 import urllib, json
 import urllib.request
 from pathlib import Path
+import datefinder
+import datetime
+import pytz
 
 filename='snapshots.json'
 
@@ -31,8 +34,17 @@ for snapshot in snapshots:
 for network, snapshots in snapshots_per_network.items():
     network_latest_snapshots = {}
     network_snapshots = {}
+    last_tezos_build_datetime=datetime.datetime(1900,1,1,tzinfo=pytz.UTC)
     for (type, mode, path) in [("tarball", "rolling", "rolling-tarball"), ("tarball", "archive", "archive-tarball"), ("tezos-snapshot", "rolling", "rolling")]:
-        typed_snapshots = [s for s in snapshots if s["artifact_type"] == type and s["history_mode"] == mode]
+        # Figure out latest tezos build we have available
+        for snapshot in snapshots:
+            matches=datefinder.find_dates(snapshot['tezos_version'])
+            tezos_build_datetime=list(matches)[0]
+            if tezos_build_datetime > last_tezos_build_datetime:
+                latest_tezos_build_version=[src for time, src in datefinder.find_dates(snapshot['tezos_version'], source=True)][1]
+                last_tezos_build_datetime=tezos_build_datetime
+        # Filter latest version for latest snapshots
+        typed_snapshots = [s for s in snapshots if s["artifact_type"] == type and s["history_mode"] == mode and latest_tezos_build_version in s['tezos_version']]
         typed_snapshots.sort(key=lambda x: int(x["block_height"]), reverse=True)
         try:
             network_latest_snapshots[path] = typed_snapshots[0]
@@ -43,6 +55,7 @@ for network, snapshots in snapshots_per_network.items():
         { "name": network, "permalink": network+"/index.html", "latest_snapshots": network_latest_snapshots })
     all_snapshots.append(
         { "name": network, "permalink": network+"/list.html", "snapshots": network_snapshots })
+    print(network+" "+latest_tezos_build_version)
 
 Path("_data").mkdir(parents=True, exist_ok=True)
 with open(f"_data/snapshot_jekyll_data.json", 'w') as f:
