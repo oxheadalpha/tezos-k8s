7f779ebf7d472c06bca5972f583ec8a716135f0a
Jimmy <5608027+orcutt989@users.noreply.github.com>
2022-10-11 12:27:24 -0400

    Snapshot Engine - Generate snapshot website from metadata with permalinks (#483)
    * initial logic piping in allSubdomains
    
    * python script for building main artifact json
    
    * fix python script
    
    * needs new jq
    
    * fix args too long error skip snapshots.json
    
    * move snapshot.json upload out of python
    
    * fix waits
    
    * cleanup zip and upload comments
    
    * add site building python and switch to monosite bucket
    
    * proper gemfile
    
    * build pages and metadata with python
    
    * rename
    
    * local snapshots.json instead of remote
    
    * parameterize theme
    
    * remove unused values and update docs
    
    * fix envs to envfrom for easier configmap mount
    
    * remove references to specific snapshot website
    
    * excepet if network is missing
    
    * except if 404
    
    * fix repo and python
    
    * fix json read
    
    * add redir from network.website to website/network
    
    * revert old wait logic
    
    * rm sleep
    
    * try sleep
    
    * add checks back with sleep and fix metadata
    
    * fix comment
    
    * uid of tezos user changed in v14 sw to username
    
    * forgot some chowns
    
    * Pin kubectl
    
    * export all artifacts in jekyll metadata (#485)
    
    This allows for the creation of a file explorer.
    
    A basic file explorer can be deployed by merging the PR:
    
    https://github.com/oxheadalpha/xtz-shots-website/pull/14
    
    Before this goes to mainnet, this file should probably be renamed from
    `getLtestSnapshotMetadata.py` to `generateJekyllData.py` since it's no
    longer only about latest artifacts.
    
    When we have v13 and v14 namespaces, the v13 and v14 artifacts will
    seamlessly be mixed together in the same file explorer page. This is as
    desired.
    
    After we deploy the file explorer and v13/v14 artifacts, let's send this
    tweet:
    
    > Tezos Kathmandu protocol will activate on <date>. You must upgrade your
    node to Octez v14.
    > Upgrading your node requires a storage migration step. To make
    migration easier for you, snapshots and tarballs for both v13 and v14
    are now available on XTZ-Shots.io
    > You can easily browse all available artifacts with our new file
    explorer:
    
    Attach a small video of the front page and file explorer navigation flow
    to the tweet.
    
    * SnapshotEngine: Check for stuck snapshots and delete (#487)
    
    * Snapshot Engine: Delete snapshots older than 6m
    
    * Tezos: New init container to upgrade storage (#486)
    
    * Tezos: Add upgrade storage init container
    
    * add expects
    
    * fix for v14 namespace buckets
    
    * extra space
    
    * fix s3 bucket refs
    
    * only serve artifacts for the newest octez per network
    
    * add pip packages
    
    * split commands
    
    * fix for old snapshots not showing on all page
    
    * rename snapshots.json
    
    * increase expected size for multipart upload
    
    * remove gemfile since its ovewritten anyways
    
    * Update upgrade-storage.sh
    
    * new expects
    
    * cleanup
    
    * serve old build instead of newest build
    
    * Add back gemfile to save website build time
    
    * hypens not underscores
    
    * rm extensions
    
    * Add gemfiles
    
    * fix gemfile
    
    * empty string in gemfile
    
    * fix gem lock file
    
    * gemfile needs later version and generator
    
    * lockfile
    
    * add webrick
    
    * this gemfile has to work
    
    Co-authored-by: orcutt989 <jimmy.orcutt@tqtezos.com>
    Co-authored-by: Nicolas Ochem <nicolas.ochem@gmail.com>

---
+++ snapshotEngine/getAllSnapshotMetadata.py
@@ -0,0 +1,30 @@
+from genericpath import exists
+import os
+import urllib, json
+import urllib.request
+
+allSubDomains = os.environ['ALL_SUBDOMAINS'].split(",")
+snapshotWebsiteBaseDomain = os.environ['SNAPSHOT_WEBSITE_DOMAIN_NAME']
+
+filename="tezos-snapshots.json"
+
+# Write empty top-level array to initialize json
+json_object = []
+
+print(allSubDomains)
+
+# Get each subdomain's base.json and combine all artifacts into 1 metadata file
+for subDomain in allSubDomains:
+  baseJsonUrl="https://"+subDomain+"."+snapshotWebsiteBaseDomain+"/base.json"
+  try:
+    with urllib.request.urlopen(baseJsonUrl) as url:
+      data = json.loads(url.read().decode())
+      for entry in data:
+        json_object.append(entry)
+  except urllib.error.HTTPError:
+    continue
+
+# Write to file
+with open (filename, 'w') as json_file:
+  json_string = json.dumps(json_object, indent=4)
+  json_file.write(json_string)
