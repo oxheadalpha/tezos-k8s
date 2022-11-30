bb5767141f85aad7ecb48deae74be00104067bf4
Nicolas Ochem <nicolas.ochem@gmail.com>
2022-11-29 23:04:12 -0800

    remove old style baking file

--- utils/config-generator.sh
+++ utils/config-generator.sh
@@ -39,33 +39,3 @@ EOM
 < /etc/tezos/config.json jq -r '.p2p."bootstrap-peers"[]'	| \
 	tr '\012' ',' | sed s/^/--bootstrap-lookup-address=/	  \
 		>> /etc/tezos/tezedge.conf
-
-#
-# Next we write the current baker account into /etc/tezos/baking-account.
-# We do it here because we shall use jq to process some of the environment
-# variables and we are not guaranteed to have jq available on an arbitrary
-# tezos docker image.
-
-MY_CLASS=$(echo $NODES | jq -r ".\"${MY_NODE_CLASS}\"")
-AM_I_BAKER=0
-if [ "$MY_CLASS" != null ]; then
-    AM_I_BAKER=$(echo $MY_CLASS | \
-		 jq -r '.runs|map(select(. == "baker"))|length')
-fi
-
-if [ "$AM_I_BAKER" -eq 1 ]; then
-    my_baker_account=$(echo $MY_CLASS | \
-	    jq -r ".instances[${MY_POD_NAME#$MY_NODE_CLASS-}]
-		   |if .bake_using_accounts
-		    then .bake_using_accounts[]
-		    else .bake_using_account
-		    end")
-
-    # If no account to bake for was specified in the node's settings,
-    # config-generator defaults the account name to the pod's name.
-    if [ "$my_baker_account" = null ]; then
-	my_baker_account="$MY_POD_NAME"
-    fi
-
-    echo "$my_baker_account" > /etc/tezos/baker-account
-fi
