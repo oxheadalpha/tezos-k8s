8fcef468e606884cfc9d1476f834608e2de344bb
Nicolas Ochem <nicolas.ochem@gmail.com>
2022-12-02 14:46:49 -0800

    remove traces of tezedge (#512)
    * remove traces of tezedge
    
    Tezedge has been deprecated, removing this option from tezos-k8s.
    
    * switch else statement per review

--- utils/config-generator.sh
+++ utils/config-generator.sh
@@ -10,36 +10,6 @@ chmod -R 777 /var/tezos
 set -e
 python3 /config-generator.py "$@"
 set +e
-
-#
-# Generate the tezedge configuration file:
-
-cat > /etc/tezos/tezedge.conf <<EOM
---network=custom
---custom-network-file=/etc/tezos/config.json
---p2p-port=9732
---rpc-port=8732
---init-sapling-spend-params-file=/sapling-spend.params
---init-sapling-output-params-file=/sapling-output.params
---tezos-data-dir=/var/tezos/node/data
---bootstrap-db-path=/var/tezos/node/bootstrap
---identity-file=/tmp/tezedge/identity.json
---identity-expected-pow=0
---log-format=simple
---log-level=info
---ocaml-log-enabled=false
---peer-thresh-low=10
---peer-thresh-high=15
---protocol-runner=/protocol-runner
---tokio-threads=0
---enable-testchain=false
---log=terminal
-EOM
-
-< /etc/tezos/config.json jq -r '.p2p."bootstrap-peers"[]'	| \
-	tr '\012' ',' | sed s/^/--bootstrap-lookup-address=/	  \
-		>> /etc/tezos/tezedge.conf
-
 #
 # Next we write the current baker account into /etc/tezos/baking-account.
 # We do it here because we shall use jq to process some of the environment
