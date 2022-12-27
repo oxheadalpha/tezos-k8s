aa35ff334a6783ec60c9a79eddb2caa1daab9a27
Nicolas Ochem <nicolas.ochem@gmail.com>
2022-12-27 14:02:02 -0800

    snapshot importer: verify there is a snapshot to import first

--- charts/tezos/scripts/snapshot-importer.sh
+++ charts/tezos/scripts/snapshot-importer.sh
@@ -6,13 +6,14 @@ node_dir="$data_dir/node"
 node_data_dir="$node_dir/data"
 node="$bin_dir/octez-node"
 snapshot_file=${node_dir}/chain.snapshot
-if [ -f ${node_dir}/chain.snapshot.block_hash ]; then
-    block_hash=$(cat ${node_dir}/chain.snapshot.block_hash)
-fi
 
 if [ ! -f ${snapshot_file} ]; then
     echo "No snapshot to import."
     exit 0
+fi
+
+if [ -f ${node_dir}/chain.snapshot.block_hash ]; then
+    block_hash=$(cat ${node_dir}/chain.snapshot.block_hash)
 fi
 
 if [ -d ${node_data_dir}/context ]; then
