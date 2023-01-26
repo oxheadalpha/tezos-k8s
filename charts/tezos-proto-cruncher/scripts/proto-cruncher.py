47fd4a437650a727f7b5c60173eaf3743c6403e0
Nicolas Ochem <nicolas.ochem@gmail.com>
2023-01-26 15:23:58 -0800

    tezos-proto-cruncher chart (#532)
    * tezos-proto-cruncher chart
    
    This chart deploys a daemonset in your cluster. the daemonset runs a
    parallel search for a vanity protocol hash.
    
    It takes s3 bucket credentials. Protocol is downloaded from bucket then
    results are written to the bucket.
    
    It does not terminate: it keeps running until the cluster is torn down.
    
    * better variable names
    
    * Update charts/tezos-proto-cruncher/scripts/proto-downloader.py
    
    Co-authored-by: Aryeh Harris <harryttd@users.noreply.github.com>
    
    * address review comments
    
    * lint python scripts with black
    
    * catch exception when s3 upload fails
    
    * fix manual method
    
    * fix manual method, better way per Aryeh
    
    * fail fast when env vars not set in proto downloader
    
    Co-authored-by: Aryeh Harris <harryttd@users.noreply.github.com>

---
+++ charts/tezos-proto-cruncher/scripts/proto-cruncher.py
@@ -0,0 +1,79 @@
+import hashlib
+import random
+import base58
+import string
+import os
+import sys
+import re
+
+
+proto_file = sys.argv[1]
+
+
+def tb(l):
+    return b"".join(map(lambda x: x.to_bytes(1, "big"), l))
+
+
+proto_prefix = tb([2, 170])
+
+PROTO_NAME = os.getenv("PROTO_NAME")
+VANITY_STRING = os.getenv("VANITY_STRING")
+NUM_NONCE_DIGITS = int(os.getenv("NUM_NONCE_DIGITS", 16))
+BUCKET_NAME = os.getenv("BUCKET_NAME")
+BUCKET_ENDPOINT_URL = os.getenv("BUCKET_ENDPOINT_URL")
+BUCKET_REGION = os.getenv("BUCKET_REGION")
+
+if not VANITY_STRING:
+    raise ValueError("VANITY_STRING env var must be set")
+
+if BUCKET_NAME:
+    import boto3
+
+    s3 = boto3.resource(
+        "s3", region_name=BUCKET_REGION, endpoint_url=f"https://{BUCKET_ENDPOINT_URL}"
+    )
+
+with open(proto_file, "rb") as f:
+    proto_bytes = f.read()
+
+with open(proto_file, "rb") as f:
+    proto_lines = f.readlines()
+
+
+# For speed, we precompute the hash of the proto without the last line
+# containing the vanity nonce.
+# Later on, we add the last line and recompute.
+# This speeds up the brute force by a large factor, compared to hashing
+# in full at every try.
+original_nonce = proto_lines[-1]
+# First 4 bytes are truncated (not used in proto hashing)
+proto_hash = hashlib.blake2b(proto_bytes[4:][: -len(original_nonce)], digest_size=32)
+
+
+def get_hash(vanity_nonce, proto_hash):
+    proto_hash.update(vanity_nonce)
+    return base58.b58encode_check(proto_prefix + proto_hash.digest()).decode("utf-8")
+
+
+print(
+    f"Original proto nonce: {original_nonce} and hash: {get_hash(original_nonce, proto_hash.copy())}"
+)
+
+while True:
+    # Warning - assuming the nonce is 16 chars in the original proto.
+    # If it is not, make sure to set NUM_NONCE_DIGITS to the right number
+    # otherwise you will get bad nonces.
+    new_nonce_digits = "".join(
+        random.choice(string.digits) for _ in range(NUM_NONCE_DIGITS)
+    )
+    new_nonce = b"(* Vanity nonce: " + bytes(new_nonce_digits, "utf-8") + b" *)\n"
+
+    new_hash = get_hash(new_nonce, proto_hash.copy())
+    if re.match(f"^{VANITY_STRING}.*", new_hash):
+        print(f"Found vanity nonce: {new_nonce} and hash: {new_hash}")
+        if BUCKET_NAME:
+            try:
+                s3.Object(BUCKET_NAME, f"{PROTO_NAME}_{new_hash}").put(Body=new_nonce)
+            except:
+                print("ERROR: upload of the nonce and hash to s3 failed.")
+                continue
--- docs/04-other-helm-charts.md
+++ docs/04-other-helm-charts.md
@@ -7,3 +7,4 @@ The Tezos-k8s project also maintains several other helm charts:
 * **[tezos-faucet](https://github.com/oxheadalpha/tezos-k8s/tree/master/charts/tezos-faucet)**: a faucet to dispense testnet funds
 * **[snapshotEngine](https://github.com/oxheadalpha/tezos-k8s/tree/master/charts/snapshotEngine)**: an utility to generate Tezos snapshots from running nodes, using Kubernetes CSI driver
 * **[tezos-reward-distributor](https://github.com/oxheadalpha/tezos-k8s/tree/master/charts/tezos-reward-distributor)**: a chart to deploy [TRD](https://tezos-reward-distributor-organization.github.io/tezos-reward-distributor/)
+* **[tezos-proto-cruncher](https://github.com/oxheadalpha/tezos-k8s/tree/master/charts/tezos-proto-cruncher)**: a chart to deploy a daemonset in your cluster to find vanity hashes for protocols in parallel
