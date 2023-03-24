9a0dab676db9b2189a6fde641131f48b2c6415b3
Nicolas Ochem <nicolas.ochem@gmail.com>
2023-03-24 13:27:56 -0700

    Update charts/tezos/scripts/upgrade-storage.sh
    Co-authored-by: Aryeh Harris <harryttd@users.noreply.github.com>

--- charts/tezos/scripts/upgrade-storage.sh
+++ charts/tezos/scripts/upgrade-storage.sh
@@ -2,7 +2,7 @@ set -ex
 
 if [ ! -e /var/tezos/node/data/context/store.dict ]
 then
-  printf "No context in data dir found, probably initial start, doing nothing."
+  printf "No store in data dir found, probably initial start, doing nothing."
   exit 0
 fi
 octez-node upgrade storage --config /etc/tezos/config.json
