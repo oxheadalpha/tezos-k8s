728b4a39da89984300e7fc70aa54956fa7c64486
Nicolas Ochem <nicolas.ochem@gmail.com>
2023-03-24 15:07:33 -0700

    create an export file for trd bucket upload secrets

--- charts/tezos-reward-distributor/scripts/bucket_upload.sh
+++ charts/tezos-reward-distributor/scripts/bucket_upload.sh
@@ -2,8 +2,8 @@
 
 echo "Uploading TRD data to bucket"
 
+source /trd/config/bucket_upload_secrets
 if [ ! -z ${BUCKET_NAME} ];then
-    export AWS_SECRET_ACCESS_KEY=$(cat /trd/config/aws_secret_access_key)
     aws s3  cp --recursive  /trd/ s3://${BUCKET_NAME}/${BAKER_NAME} --endpoint $BUCKET_ENDPOINT_URL
 fi
 sleep 10
