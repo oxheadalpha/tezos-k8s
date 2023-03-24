525a16773b05caf1cc81224bcbd5ac6632587ebb
Nicolas Ochem <nicolas.ochem@gmail.com>
2023-03-24 16:22:21 -0700

    fix wrong path

--- charts/tezos-reward-distributor/scripts/bucket_upload.sh
+++ charts/tezos-reward-distributor/scripts/bucket_upload.sh
@@ -2,7 +2,7 @@
 
 echo "Uploading TRD data to bucket"
 
-source /trd/config/bucket_upload_secrets
+source /trd/cfg/bucket_upload_secrets
 if [ ! -z ${BUCKET_NAME} ];then
     aws s3  cp --recursive  /trd/ s3://${BUCKET_NAME}/${BAKER_NAME} --endpoint $BUCKET_ENDPOINT_URL
 fi
