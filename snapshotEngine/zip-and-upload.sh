251c257004d5b0f050fd2b070585c1e59c020607
orcutt989 <orcutt989@gmail.com>
2023-04-05 13:38:50 -0400

    show commands for debug

--- snapshotEngine/zip-and-upload.sh
+++ snapshotEngine/zip-and-upload.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -o xtrace
+
 BLOCK_HEIGHT=$(cat /"${HISTORY_MODE}"-snapshot-cache-volume/BLOCK_HEIGHT)
 BLOCK_HASH=$(cat /"${HISTORY_MODE}"-snapshot-cache-volume/BLOCK_HASH)
 BLOCK_TIMESTAMP=$(cat /"${HISTORY_MODE}"-snapshot-cache-volume/BLOCK_TIMESTAMP)
