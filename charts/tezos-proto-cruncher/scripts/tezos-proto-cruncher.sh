cf7bea58305b10842940062d4854ff4502919d6f
Nicolas Ochem <nicolas.ochem@gmail.com>
2023-04-10 22:33:33 -0700

    rework proto cruncher to use new rust tz-proto-vanity (#575)
    The previous proto cruncher was in python and embedded in this helm
    chart, running in the utils container. Our new one leverages Igor's
    project:
    
    https://github.com/oxheadalpha/tz-proto-vanity

--- charts/tezos-proto-cruncher/scripts/tezos-proto-cruncher.sh
+++ charts/tezos-proto-cruncher/scripts/tezos-proto-cruncher.sh
@@ -1,11 +1,6 @@
 set -eo pipefail
-apk add parallel
+apk add py3-pip
 pip install boto3
 
-python /proto-downloader.py
-
-if [ -z "${NUM_PARALLEL_PROCESSES}" ]; then
-    # Launch one process per CPU core to maximize utilization
-    NUM_PARALLEL_PROCESSES=$(grep -c ^processor /proc/cpuinfo)
-fi
-seq $NUM_PARALLEL_PROCESSES | parallel --ungroup python /proto-cruncher.py /${PROTO_NAME}
+export PYTHONUNBUFFERED=1
+python /tezos-proto-cruncher.py
