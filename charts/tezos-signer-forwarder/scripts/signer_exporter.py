82906a6e6245fc327ef6f8e6e0832f653e0144a7
Nicolas Ochem <nicolas.ochem@gmail.com>
2023-04-11 15:14:29 -0700

    last part - replace ad-hoc relabeling with proper ServiceMonitor config

--- charts/tezos-signer-forwarder/scripts/signer_exporter.py
+++ charts/tezos-signer-forwarder/scripts/signer_exporter.py
@@ -12,25 +12,7 @@ application = Flask(__name__)
 
 readiness_probe_path = os.getenv("READINESS_PROBE_PATH")
 signer_port = os.getenv("SIGNER_PORT")
-endpoint_alias = os.getenv("ENDPOINT_ALIAS")
 signer_metrics = os.getenv("SIGNER_METRICS") == "true"
-
-def relabel(prometheus_metrics,extra_labels):
-    '''
-    Add labels to existing prometheus_metrics
-    '''
-    relabeled_metrics = ""
-    for line in prometheus_metrics.splitlines():
-        if line.startswith("#"):
-            relabeled_metrics += line + "\n"
-        else:
-            if "{" in line:
-                # line has labels 
-                labeled_line = re.sub(r'{(.*)}', r'{\1,%s}' % extra_labels, line)
-            else:
-                labeled_line = re.sub(r'(.*) ', r'\1{%s} ' % extra_labels, line)
-            relabeled_metrics += labeled_line + "\n"
-    return relabeled_metrics
 
 @application.route('/metrics', methods=['GET'])
 def prometheus_metrics():
@@ -39,7 +21,6 @@ def prometheus_metrics():
     Combines the readiness probe URL (i.e. checks that ledger is configured)
     and the health probe (check power status for example)
     '''
-    extra_labels = 'midl_endpoint_alias="%s"' % endpoint_alias
 
     try:
         probe = requests.get(f"http://localhost:{signer_port}{readiness_probe_path}")
@@ -47,14 +28,14 @@ def prometheus_metrics():
         probe = None
     if probe and signer_metrics:
         try:
-            healthz = relabel(requests.get(f"http://localhost:{signer_port}/healthz").text, extra_labels)
+            healthz = requests.get(f"http://localhost:{signer_port}/healthz").text
         except requests.exceptions.RequestException:
             healthz = None
     else:
         healthz = None
     return '''# number of unhealthy signers - should be 0 or 1
-unhealthy_signers_total{%s} %s
-%s''' % (extra_labels, 0 if probe else 1, healthz or "")
+unhealthy_signers_total %s
+%s''' % (0 if probe else 1, healthz or "")
 
 if __name__ == "__main__":
    application.run(host = "0.0.0.0", port = 31732, debug = False)
