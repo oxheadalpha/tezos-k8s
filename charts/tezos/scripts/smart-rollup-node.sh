a538c024cb2907d74141c1f364dd0b5d9ec93bfa
Nicolas Ochem <nicolas.ochem@gmail.com>
2023-04-14 22:44:54 -0700

    fix rpc endpoint

--- charts/tezos/scripts/smart-rollup-node.sh
+++ charts/tezos/scripts/smart-rollup-node.sh
@@ -6,8 +6,7 @@ CLIENT_DIR="$TEZ_VAR/client"
 NODE_DIR="$TEZ_VAR/node"
 NODE_DATA_DIR="$TEZ_VAR/node/data"
 
-CMD="$TEZ_BIN/octez-smart-rollup-node-alpha -d $CLIENT_DIR run operator for ${ROLLUP_ADDRESS} with operators ${OPERATOR_ACCOUNT}"
-
+CMD="$TEZ_BIN/octez-smart-rollup-node-alpha --endpoint http://tezos-node-rpc:8732 -d $CLIENT_DIR run operator for ${ROLLUP_ADDRESS} with operators ${OPERATOR_ACCOUNT}"
 # ensure we can run tezos-signer commands without specifying client dir
 ln -s /var/tezos/client /home/tezos/.tezos-signer
 
