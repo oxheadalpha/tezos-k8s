f17a9d25f0a6551073a098da180fd1aa1bc8de05
Nicolas Ochem <nicolas.ochem@gmail.com>
2023-05-30 20:12:38 -0700

    make rollup node listen on 0.0.0.0

--- charts/tezos/scripts/smart-rollup-node.sh
+++ charts/tezos/scripts/smart-rollup-node.sh
@@ -7,6 +7,11 @@ NODE_DIR="$TEZ_VAR/node"
 NODE_DATA_DIR="$TEZ_VAR/node/data"
 
 touch /var/tezos/smart-rollup-boot-sector
-CMD="$TEZ_BIN/octez-smart-rollup-node-alpha --endpoint http://tezos-node-rpc:8732 -d $CLIENT_DIR run operator for ${ROLLUP_ADDRESS} with operators ${OPERATOR_ACCOUNT} --boot-sector-file /var/tezos/smart-rollup-boot-sector"
+CMD="$TEZ_BIN/octez-smart-rollup-node-alpha \
+  --endpoint http://tezos-node-rpc:8732 \
+  -d $CLIENT_DIR \
+  run operator for ${ROLLUP_ADDRESS} with operators ${OPERATOR_ACCOUNT} \
+  --boot-sector-file /var/tezos/smart-rollup-boot-sector \
+  --rpc-addr 0.0.0.0"
 
 exec $CMD
