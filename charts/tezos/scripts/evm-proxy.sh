eecc09b21d09489a9ffe6c9426bed46b2a92c3c3
Nicolas Ochem <nicolas.ochem@gmail.com>
2023-06-01 09:31:39 -0700

    add ingress for evm proxy

--- charts/tezos/scripts/evm-proxy.sh
+++ charts/tezos/scripts/evm-proxy.sh
@@ -2,6 +2,8 @@ set -ex
 
 TEZ_BIN=/usr/local/bin
 
-CMD="$TEZ_BIN/octez-evm-proxy-server run"
+CMD="$TEZ_BIN/octez-evm-proxy-server run \
+  --rpc-addr 0.0.0.0 \
+  --rollup-node-endpoint  http://rollup-evm:8932"
 
 exec $CMD
