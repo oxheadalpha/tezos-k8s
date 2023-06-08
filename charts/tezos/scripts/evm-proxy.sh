b48dd976bde2ae5c4c906a02cc9ce7587fc7e602
Nicolas Ochem <nicolas.ochem@gmail.com>
2023-06-08 08:39:31 -0700

    support several evm rollups

--- charts/tezos/scripts/evm-proxy.sh
+++ charts/tezos/scripts/evm-proxy.sh
@@ -4,6 +4,6 @@ TEZ_BIN=/usr/local/bin
 
 CMD="$TEZ_BIN/octez-evm-proxy-server run \
   --rpc-addr 0.0.0.0 \
-  --rollup-node-endpoint  http://rollup-evm:8932"
+  --rollup-node-endpoint  http://rollup-${MY_POD_NAME}:8932"
 
 exec $CMD
