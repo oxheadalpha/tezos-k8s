cfbaa3023fb15a20ea56ed253032eea24ce52c32
Nicolas Ochem <nicolas.ochem@gmail.com>
2023-06-09 16:47:30 -0700

    DAL initial

---
+++ charts/tezos/scripts/dal-node.sh
@@ -0,0 +1,18 @@
+set -ex
+
+TEZ_VAR=/var/tezos
+TEZ_BIN=/usr/local/bin
+DAL_DATA_DIR="$TEZ_VAR/dal"
+
+mkdir -p ${DAL_DATA_DIR}
+$TEZ_BIN/octez-dal-node \
+  init-config \
+  --data-dir ${DAL_DATA_DIR} \
+  --rpc-addr 0.0.0.0
+
+CMD="$TEZ_BIN/octez-dal-node \
+  --endpoint http://tezos-node-rpc:8732 \
+  run \
+  --data-dir ${DAL_DATA_DIR}"
+
+exec $CMD
