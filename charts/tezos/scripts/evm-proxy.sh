8a2d0ab689a485e75d8aa4b37ed5cb44fc5dd94c
Nicolas Ochem <nicolas.ochem@gmail.com>
2023-06-11 17:38:21 -0700

    new syntax for evm proxy

--- charts/tezos/scripts/evm-proxy.sh
+++ charts/tezos/scripts/evm-proxy.sh
@@ -3,7 +3,7 @@ set -ex
 TEZ_BIN=/usr/local/bin
 
 CMD="$TEZ_BIN/octez-evm-proxy-server run \
-  --rpc-addr 0.0.0.0 \
-  --rollup-node-endpoint  http://rollup-${MY_POD_NAME}:8932"
+  with endpoint http://rollup-${MY_POD_NAME}:8932 \
+  --rpc-addr 0.0.0.0"
 
 exec $CMD
