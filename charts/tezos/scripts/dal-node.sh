ffda22e7f632613b013b1ba8f8e1d93204e5cfce
Nicolas Ochem <nicolas.ochem@gmail.com>
2023-06-15 09:04:01 -0700

    dal node new syntax

--- charts/tezos/scripts/dal-node.sh
+++ charts/tezos/scripts/dal-node.sh
@@ -5,17 +5,13 @@ TEZ_BIN=/usr/local/bin
 DAL_DATA_DIR="$TEZ_VAR/dal"
 
 mkdir -p ${DAL_DATA_DIR}
-$TEZ_BIN/octez-dal-node \
-  init-config \
-  --data-dir ${DAL_DATA_DIR} \
-  --net-addr 0.0.0.0:11732 \
-  --rpc-addr 0.0.0.0 \
-  --rpc-port 10732 \
-  --use-unsafe-srs-for-tests
 
 CMD="$TEZ_BIN/octez-dal-node \
-  --endpoint http://tezos-node-rpc:8732 \
   run \
-  --data-dir ${DAL_DATA_DIR}"
+  --data-dir ${DAL_DATA_DIR} \
+  --endpoint http://tezos-node-rpc:8732 \
+  --net-addr 0.0.0.0:11732 \
+  --rpc-addr 0.0.0.0:10732 \
+  --use-unsafe-srs-for-tests"
 
 exec $CMD
