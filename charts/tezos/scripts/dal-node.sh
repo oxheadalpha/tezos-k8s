bf877fb3f008304f6daf9c19cd979afe3f73bbe7
Nicolas Ochem <nicolas.ochem@gmail.com>
2023-07-28 14:03:09 -0700

    support for bootstrap profile in DAL

--- charts/tezos/scripts/dal-node.sh
+++ charts/tezos/scripts/dal-node.sh
@@ -6,8 +6,15 @@ DAL_DATA_DIR="$TEZ_VAR/dal"
 
 mkdir -p ${DAL_DATA_DIR}
 
+extra_args=""
+if [ ${BOOTSTRAP_PROFILE} == "true" ]; then
+  extra_args="--bootstrap-profile"
+fi
+
+
 CMD="$TEZ_BIN/octez-dal-node \
   run \
+  ${extra_args} \ 
   --data-dir ${DAL_DATA_DIR} \
   --endpoint http://tezos-node-rpc:8732 \
   --net-addr 0.0.0.0:11732 \
