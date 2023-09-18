945e698f4170b1f8a02848c353adee2e7260ff5f
Nicolas Ochem <nicolas.ochem@gmail.com>
2023-09-17 19:30:58 -0700

    switch to universal smart rollup node (instead of proto-dependent)

--- charts/tezos/scripts/smart-rollup-node.sh
+++ charts/tezos/scripts/smart-rollup-node.sh
@@ -9,7 +9,7 @@ ROLLUP_DATA_DIR_PREIMAGES="$ROLLUP_DATA_DIR/wasm_2_0_0"
 xxd -p -c 0 /usr/local/share/tezos/evm_kernel/evm_installer.wasm | tr -d '\n' > /var/tezos/smart-rollup-boot-sector
 mkdir -p "$ROLLUP_DATA_DIR_PREIMAGES"
 cp /usr/local/share/tezos/evm_kernel/* "$ROLLUP_DATA_DIR_PREIMAGES"
-CMD="$TEZ_BIN/octez-smart-rollup-node-alpha \
+CMD="$TEZ_BIN/octez-smart-rollup-node \
   --endpoint http://tezos-node-rpc:8732 \
   -d $CLIENT_DIR \
   run operator for ${ROLLUP_ADDRESS} with operators ${OPERATOR_ACCOUNT} \
