191040e463354acb9033a5ca337caa95f5777bcb
Nicolas Ochem <nicolas.ochem@gmail.com>
2023-10-31 08:46:51 -0700

    actually mondaynet doesn't have evm rollup

--- charts/tezos/scripts/evm-proxy.sh
+++ charts/tezos/scripts/evm-proxy.sh
@@ -2,17 +2,9 @@ set -ex
 
 TEZ_BIN=/usr/local/bin
 
-if ls /usr/local/bin/octez-evm-node; then
-  CMD="$TEZ_BIN/octez-evm-node run proxy \
-    with endpoint http://rollup-${MY_POD_NAME}:8932 \
-    --mode dev \
-    --rpc-addr 0.0.0.0"
-else
-  # temporary until 20231106 mondaynet - old command
-  CMD="$TEZ_BIN/octez-evm-proxy-server run \
-    with endpoint http://rollup-${MY_POD_NAME}:8932 \
-    --mode dev \
-    --rpc-addr 0.0.0.0"
-fi
+CMD="$TEZ_BIN/octez-evm-node run proxy \
+  with endpoint http://rollup-${MY_POD_NAME}:8932 \
+  --mode dev \
+  --rpc-addr 0.0.0.0"
 
 exec $CMD
