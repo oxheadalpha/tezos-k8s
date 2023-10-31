ea864f006c578b4526ab4cfd7d7a72f8de0acde8
Nicolas Ochem <nicolas.ochem@gmail.com>
2023-10-31 08:43:30 -0700

    new evm proxy CLI

--- charts/tezos/scripts/evm-proxy.sh
+++ charts/tezos/scripts/evm-proxy.sh
@@ -2,9 +2,17 @@ set -ex
 
 TEZ_BIN=/usr/local/bin
 
-CMD="$TEZ_BIN/octez-evm-proxy-server run \
-  with endpoint http://rollup-${MY_POD_NAME}:8932 \
-  --mode dev \
-  --rpc-addr 0.0.0.0"
+if ls /usr/local/bin/octez-evm-node; then
+  CMD="$TEZ_BIN/octez-evm-node run proxy \
+    with endpoint http://rollup-${MY_POD_NAME}:8932 \
+    --mode dev \
+    --rpc-addr 0.0.0.0"
+else
+  # temporary until 20231106 mondaynet - old command
+  CMD="$TEZ_BIN/octez-evm-proxy-server run \
+    with endpoint http://rollup-${MY_POD_NAME}:8932 \
+    --mode dev \
+    --rpc-addr 0.0.0.0"
+fi
 
 exec $CMD
