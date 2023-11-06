9d1750c5b1a785097d3ca06d470d4e431371c7bb
Nicolas Ochem <nicolas.ochem@gmail.com>
2023-11-06 12:48:09 -0800

    Signer authorized keys (#610)
    * Support for Signer Authorized Keys
    
    Authorized key is the tezos native method to authenticate signing
    requests, one that we use in the new tezos-kms-signer-lambda.
    
    This adds the required support on tezos-k8s to sign with such a signer.
    
    The way it works in octez is:
    
    * when the baker/client connects to the signer for the first time,
      signer answers with a list of "authorized_keys" that the signature
      request must be signed with. These authorized keys are just tezos
      accounts
    * if the baker/client has the secret key for one of these authorized
      keys, they will just sign every request with it. otherwise, there will
      be an error
    * this can't be nested. the authorized_key can't be remote
    
    We add support in tezos-k8s by assuming the authorized_keys are just
    standard "accounts". Then, you may configure a baker as follows:
    
    ```
    nodes:
      mybaker:
        bake_using_accounts:
        - mybakeraddy
        authorized_keys:
        - my_authorized_key
    ```
    
    config-generator then ensures that the private authorized key is
    accessible to the baker.
    
    We also add support on octez-signer end:
    
    ```
    octezSigners:
      mysigner:
        sign_for_accounts:
        - mybakeraddy
        authorized_keys:
        - my_authorized_key
    ```
    
    When set, the signer mandates requests to be authenticated. Otherwise,
    it signs anything.
    
    This way, you can test end-to-end in a private chain.
    
    We modify mkchain to do this by default: mkchain now generates an
    authorized key and uses it to sign by default.
    
    Also, mkchain was previously defaulting to using one remote signer, but
    this broke when adding support for tacoInfra signer. I fixed it.
    
    I have tested it with 3 bakers and 2 signers, one authorized and one
    not. It's all working. I haven't tried zerotier and public chains.
    
    Other changes:
    
    * switch default version to 17.3
    * no magic byte restriction from signer - prevents activation
    
    * Update mkchain/tqchain/mkchain.py
    
    Co-authored-by: Aryeh Harris <harryttd@users.noreply.github.com>
    
    * Update charts/tezos/values.yaml
    
    Co-authored-by: Aryeh Harris <harryttd@users.noreply.github.com>
    
    * comment phrasing, per review
    
    * fix comments per review
    
    * validate in helm that authroized keys exist
    
    * Update charts/tezos/templates/_helpers.tpl
    
    Co-authored-by: Aryeh Harris <harryttd@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Aryeh Harris <harryttd@users.noreply.github.com>

--- charts/tezos/scripts/remote-signer.sh
+++ charts/tezos/scripts/remote-signer.sh
@@ -6,7 +6,11 @@ CLIENT_DIR="$TEZ_VAR/client"
 NODE_DIR="$TEZ_VAR/node"
 NODE_DATA_DIR="$TEZ_VAR/node/data"
 
-CMD="$TEZ_BIN/octez-signer -d $CLIENT_DIR launch http signer --magic-bytes 0x11,0x12,0x13 --check-high-watermark -a 0.0.0.0 -p 6732"
+extra_args=""
+if [ -f ${CLIENT_DIR}/authorized_keys ]; then
+  extra_args="${extra_args} --require-authentication"
+fi
+CMD="$TEZ_BIN/octez-signer -d $CLIENT_DIR ${extra_args} launch http signer -a 0.0.0.0 -p 6732"
 
 # ensure we can run tezos-signer commands without specifying client dir
 ln -s /var/tezos/client /home/tezos/.tezos-signer
