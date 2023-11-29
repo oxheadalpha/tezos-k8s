69240ecaaa667f4bbffb52c3951544467b176db9
Nicolas Ochem <nicolas.ochem@gmail.com>
2023-11-28 19:59:48 -0800

    handle metadata website down or returning invalid json (#612)
    * handle metadata website down or returning invalid json
    
    * catch json exception explicitly

--- utils/config-generator.py
+++ utils/config-generator.py
@@ -734,7 +734,13 @@ def create_node_snapshot_config_json(history_mode):
         octez_container_version = os.environ.get("OCTEZ_VERSION")
     snapshot_source = os.environ.get("SNAPSHOT_SOURCE")
     if snapshot_source:
-        all_snapshots = requests.get(snapshot_source).json()
+        try:
+            response = requests.get(snapshot_source)
+            response.raise_for_status()  # Raises an HTTPError if the HTTP request returned an unsuccessful status code
+            all_snapshots = response.json()
+        except (requests.exceptions.RequestException, requests.exceptions.JSONDecodeError):  # Catches exceptions related to requests and invalid JSON
+            print(f"Error: unable to retrieve snapshot metadata from {snapshot_source}")
+            return
     else:
         return
     try:
