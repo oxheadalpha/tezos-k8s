8377099d46943416a1c297304bd10f969e6c5212
Nicolas Ochem <nicolas.ochem@gmail.com>
2023-12-08 16:51:26 -0800

    optional hardcoded identity for DAL nodes

--- charts/tezos/scripts/dal-node.sh
+++ charts/tezos/scripts/dal-node.sh
@@ -19,6 +19,13 @@ fi
 if [ "${PUBLIC_ADDR}" != "" ]; then
   extra_args="${extra_args} --public-addr ${PUBLIC_ADDR}"
 fi
+# populate identity, if provided
+if [ -n "$IDENTITY_JSON" ]; then
+    identity_path=/var/tezos/dal/identity.json
+    printf "Found persistent identity, writing to $identity_path"
+    echo "$IDENTITY_JSON" >  $identity_path
+fi
+#
 
 CMD="$TEZ_BIN/octez-dal-node run ${extra_args} --data-dir ${DAL_DATA_DIR} \
   --endpoint http://tezos-node-rpc:8732 \
