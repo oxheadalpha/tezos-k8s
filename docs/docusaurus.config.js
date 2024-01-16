0741be9e55c636e18bafa275383a7e05dd407e92
Nicolas Ochem <nicolas.ochem@gmail.com>
2024-01-16 07:49:55 -0800

    migrate website to own domain

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Tezos K8s',
   tagline: 'Deploy a Tezos Blockchain on Kubernetes',
-  url: 'https://tacoinfra.github.io/',
-  baseUrl: '/tezos-k8s/',
+  url: 'https://tezos-k8s.io/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
